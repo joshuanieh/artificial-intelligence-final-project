04de481170460e39d41bf7043bb82ef9d8090bf0
joshuanieh <b07502027@ntu.edu.tw>
2022-05-25 09:50:47 +0800

    initial

---
+++ train.py
@@ -0,0 +1,18 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import pandas as pd
+class ExampleDataset(Dataset):
+    def __init__(self):
+        xy = np.loadtxt('./dataset-example.csv', delimiter=',', dtype=np.float32, skiprows=1)
+        self.x = torch.from_numpy(xy[:, 1:])
+        self.y = torch.from_numpy(xy[:, [0]])
+        self.n_samples = xy.shape[0]
+
+    def __getitem__(self, index):
+        
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        
+        return self.n_samples
