5da7acd69e6db605d68e17104fbd96377724256b
blafea <b07502166@ntu.edu.tw>
2022-05-25 15:51:05 +0800

    aaa

--- train.py
+++ train.py
@@ -7,9 +7,13 @@ class ExampleDataset(Dataset):
         xy = pd.read_csv("./data/keelung.csv", encoding="Big5", skiprows=2, usecols=[i for i in range(3,27)])
         xy = xy.apply(pd.to_numeric, errors='coerce')
         print("origin data: ", xy)
-        xy = xy.mean(axis=1, skipna=True, numeric_only=True).values
-        print("mean", xy)
-        self.x = np.concatenate((xy[0:-3], xy[1:-2], xy[2:-1]), axis=1)
+        xy = xy.mean(axis=1, skipna=True, numeric_only=True)
+        print("mean: ", xy)
+        #print(xy[1:-2].T)
+        a = xy[1:-2].reset_index(drop=True)
+        b = xy[2:-1].reset_index(drop=True)
+        self.x = pd.concat([xy[0:-3].T, a.T, b.T], axis=1)
+        print(self.x)
         self.y = xy[3:]
 
     def __getitem__(self, index):        
@@ -20,7 +24,7 @@ class ExampleDataset(Dataset):
 
 dataset = ExampleDataset()
 
-first_data = dataset[0]
+'''first_data = dataset[0]
 features, labels = first_data
 print(features, labels)
-print(len(dataset))
+print(len(dataset))'''
