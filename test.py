035de2fac313ff505fab0c05515ba357a7d92906
Jimmy <jimmy881122naruto@gmail.com>
2022-05-25 12:50:50 +0800

    keelung.csv

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from distutils.log import error
 import pandas as pd
 import numpy as np
-df = pd.read_csv("./data/keelung.csv", encoding="Big5").iloc[:, 3:]
+df = pd.read_csv("./data/keelung.csv", encoding="Big5", skiprows=2, usecols=[i for i in range(3,27)])
+
+# print(df)
+# print(df.iloc[0,0])
+# df.astype(dtype='float64', errors='ignore')
 df = df.apply(pd.to_numeric, errors='coerce')
-print(df["0"].dtypes)
-print(type(df.iloc[0,0]))
-print(df.mean(axis=1, numeric_only=True))
+print(df)
+print(df.mean(axis=1, skipna=True, numeric_only=True))
