2a4b6378faecf3f198fb04d0a4266c7a83b7ffe8
blafea <b07502166@ntu.edu.tw>
2022-05-25 12:47:07 +0800

    aaa

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
+from distutils.log import error
 import pandas as pd
 import numpy as np
 df = pd.read_csv("./data/keelung.csv", encoding="Big5").iloc[:, 3:]
-
-print(df)
+df = df.apply(pd.to_numeric, errors='coerce')
+print(df["0"].dtypes)
 print(type(df.iloc[0,0]))
-print(df.mean(axis=1, skipna=True, numeric_only=True))
+print(df.mean(axis=1, numeric_only=True))
