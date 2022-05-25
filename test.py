20f2c1532044529a812470c451e9ab0528273144
joshuanieh <b07502027@ntu.edu.tw>
2022-05-25 13:54:24 +0800

    p

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import numpy as np
 df = pd.read_csv("./data/keelung.csv", encoding="Big5", skiprows=2, usecols=[i for i in range(3,27)])
-
-# print(df)
-# print(df.iloc[0,0])
-# df.astype(dtype='float64', errors='ignore')
 df = df.apply(pd.to_numeric, errors='coerce')
 print(df)
 print(df.mean(axis=1, skipna=True, numeric_only=True))
--- train.py
+++ train.py
@@ -4,9 +4,15 @@ import numpy as np
 import pandas as pd
 class ExampleDataset(Dataset):
     def __init__(self):
+        xy = pd.read_csv("./data/keelung.csv", encoding="Big5", skiprows=2, usecols=[i for i in range(3,27)])
+        xy = xy.apply(pd.to_numeric, errors='coerce')
+        print(xy)
+        print(xy.mean(axis=1, skipna=True, numeric_only=True))
         xy = np.loadtxt('./data/keelung.csv', delimiter=',', dtype=np.float32, skiprows=1)
         self.x = torch.from_numpy(xy[:, 1:])
+        # print(self.x)
         self.y = torch.from_numpy(xy[:, [0]])
+        print(self.y)
         self.n_samples = xy.shape[0]
 
     def __getitem__(self, index):        
@@ -18,7 +24,7 @@ class ExampleDataset(Dataset):
 dataset = ExampleDataset()
 
 # pick first data
-first_data = dataset[0]
-features, labels = first_data
-print(features, labels)
-print(len(dataset))
+# first_data = dataset[0]
+# features, labels = first_data
+# print(features, labels)
+# print(len(dataset))
