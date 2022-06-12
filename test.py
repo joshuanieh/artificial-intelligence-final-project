15e32acadda81cc7116124357726c19b6e5e8fe7
joshuanieh <b07502027@ntu.edu.tw>
2022-06-12 14:32:24 +0800

    clean

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.read_csv("./data/keelung.csv", encoding="Big5", skiprows=2, usecols=[i for i in range(3,27)])
-df = df.apply(pd.to_numeric, errors='coerce')
-print(df)
-print(df.mean(axis=1, skipna=True, numeric_only=True))
